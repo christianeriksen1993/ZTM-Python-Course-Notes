c4f413bdadea3e4581d7dedc99b625b36d09776c
christianeriksen1993 <christian.eriksen1993@gmail.com>
2023-12-12 20:37:20 +0100

    Finish code up until lesson 256

--- hacker-news-project/scrape.py
+++ hacker-news-project/scrape.py
@@ -1,17 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
-res = requests.get("https://news.ycombinator.com/news")
-soup = BeautifulSoup(res.text, "html.parser")
-links = soup.select(".titleline")
-votes = soup.select(".score")
+res = requests.get("https://news.ycombinator.com/?p=1")
+soup = BeautifulSoup(res.content, "html.parser")
+links = soup.select(".titleline > a")
+subtext = soup.select(".subtext")
 
-def create_custom_hn(links, votes):
+def sort_stories_by_vote(hnlist):
+    return sorted(hnlist, key=lambda k:k["votes"], reverse=True)
+
+def create_custom_hn(links, subtext):
     hn = []
     for idx, item in enumerate(links):
         title = links[idx].getText()
-        href = links[idx].get("href")
-        hn.append({"title": title, "link": href})
-    return hn
+        href = item.get("href")
+        vote = subtext[idx].select(".score")
+        if len(vote):
+            points = int(vote[0].getText().replace(" points", ""))
+            if points > 99:
+                hn.append({"title": title, "link": href, "votes": points})
+    return sort_stories_by_vote(hn)
 
-print(create_custom_hn(links, votes))
+pprint.pprint(create_custom_hn(links, subtext))
