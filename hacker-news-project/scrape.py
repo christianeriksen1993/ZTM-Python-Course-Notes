6e2d3b4485a597fb3d20e96314dc71710a547473
christianeriksen1993 <christian.eriksen1993@gmail.com>
2023-12-27 18:14:47 +0100

    Add scraping of page 2 news as well

--- hacker-news-project/scrape.py
+++ hacker-news-project/scrape.py
@@ -2,10 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 import pprint
 
+
 res = requests.get("https://news.ycombinator.com/?p=1")
+res2 = requests.get("https://news.ycombinator.com/?p=2")
 soup = BeautifulSoup(res.content, "html.parser")
+soup2 = BeautifulSoup(res2.content, "html.parser")
 links = soup.select(".titleline > a")
+links2 = soup2.select(".titleline > a")
 subtext = soup.select(".subtext")
+subtext2 = soup2.select(".subtext")
+mega_links = links + links2
+mega_subtext = subtext + subtext2
 
 def sort_stories_by_vote(hnlist):
     return sorted(hnlist, key=lambda k:k["votes"], reverse=True)
@@ -22,4 +29,4 @@ def create_custom_hn(links, subtext):
                 hn.append({"title": title, "link": href, "votes": points})
     return sort_stories_by_vote(hn)
 
-pprint.pprint(create_custom_hn(links, subtext))
+pprint.pprint(create_custom_hn(mega_links, mega_subtext))
