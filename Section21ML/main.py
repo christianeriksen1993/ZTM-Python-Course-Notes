0954d7f633f665580e54470faa49d9fff88c717a
christianeriksen1993 <christian.eriksen1993@gmail.com>
2024-07-01 14:20:41 +0200

    Add steps for ML notes

---
+++ Section21ML/main.py
@@ -0,0 +1,14 @@
+#1 - Import the data 
+#2 - Clean the data 
+#3 - Split data. Training set/Test set
+#4 - Create a Model
+#5 - Check the output
+#6 - Improve
+
+#Tools to use: 
+#NumPy for lists and arrays
+#Pandas for tabular data
+#scikit-learn for creating the model
+#Motplotlib for presenting data results
+#Jupyter notebooks for keeping track of everything we have done etc.
+#Kaggle For getting datasets etc.
